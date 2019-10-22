f82e4fe755282e23b561d1e641215610d9ae8c6a
Tulio Viglione <tulio.l.viglione@gmail.com>
2019-10-22 20:59:52 -0300

    Commit inicial

---
+++ src/main/java/com/calculo/tempo/TempoApiApplication.java
@@ -0,0 +1,13 @@
+package com.calculo.tempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calculo/tempo/TempoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculo.tempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
