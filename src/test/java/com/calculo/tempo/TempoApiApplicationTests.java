8bb402bb1f5de8eb0b772c7d023bbcc3aaf84929
Tulio Viglione <tulio.l.viglione@gmail.com>
2019-10-23 22:42:18 -0300

    definido profile test

--- src/test/java/com/calculo/tempo/TempoApiApplicationTests.java
+++ src/test/java/com/calculo/tempo/TempoApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.calculo.tempo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TempoApiApplicationTests {
 
 	@Test
